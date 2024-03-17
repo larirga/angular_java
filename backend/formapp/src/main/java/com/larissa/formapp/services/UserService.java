4643392f5d6f801d44e10b3afc2fff2ca84ac75d
larirga <larisssa_rga@hotmail.com>
2024-03-17 15:57:48 -0300

    add a seed to manage database testing

--- backend/formapp/src/main/java/com/larissa/formapp/services/UserService.java
+++ backend/formapp/src/main/java/com/larissa/formapp/services/UserService.java
@@ -5,11 +5,13 @@ import com.larissa.formapp.repositories.UserRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
   @Autowired
   private UserRepository repository;
+  @Transactional(readOnly = true)
   public List<User> findAll() {
     return repository.findAll();
   }
---
+++ backend/formapp/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_user (name, email) VALUES ('Rafael', 'rafael@email.com');
+INSERT INTO tb_user (name, email) VALUES ('Tiago', 'tiago@email.com');
+INSERT INTO tb_user (name, email) VALUES ('Marina', 'marina@email.com');
+INSERT INTO tb_user (name, email) VALUES ('Natalia', 'natalia@email.com');

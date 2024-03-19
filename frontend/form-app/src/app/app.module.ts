353ac6570ff1813d93280b002c2ffe8d3a53d207
larirga <larisssa_rga@hotmail.com>
2024-03-19 18:33:16 -0300

    associate Form with User-Input and implement validation for the name and email inputs

--- frontend/form-app/src/app/app.module.ts
+++ frontend/form-app/src/app/app.module.ts
@@ -4,19 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { UserInputComponent } from './user-input/user-input.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, UserInputComponent, UserListComponent, LayoutComponent],
+  declarations: [
+    AppComponent,
+    UserInputComponent,
+    UserListComponent,
+    LayoutComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
+    ReactiveFormsModule,
   ],
   providers: [FormsModule],
   bootstrap: [AppComponent],
--- frontend/form-app/src/app/user-input/user-input.component.ts
+++ frontend/form-app/src/app/user-input/user-input.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user-input',
@@ -6,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-input.component.css'],
 })
 export class UserInputComponent implements OnInit {
-  constructor() {}
+  public formUser: FormGroup;
+  constructor(private fb: FormBuilder, private toast: ToastrService) {
+    this.formUser = this.createFormUser();
+  }
 
   ngOnInit(): void {}
+
+  public createFormUser(): FormGroup {
+    return this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  public isFormControlInvalid(controlName: string): boolean {
+    const control = this.formUser.get(controlName);
+    return !!(control && control.invalid && (control.touched || control.dirty));
+  }
 }

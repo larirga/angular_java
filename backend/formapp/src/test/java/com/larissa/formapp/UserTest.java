6df1e4148f36b2fb1c56f191d30c7f145b9f35a8
larirga <larisssa_rga@hotmail.com>
2024-03-20 11:43:43 -0300

    add FindById test for /user

--- backend/formapp/src/test/java/com/larissa/formapp/UserTest.java
+++ backend/formapp/src/test/java/com/larissa/formapp/UserTest.java
@@ -5,7 +5,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.larissa.formapp.DTO.UserDTO;
 import com.larissa.formapp.services.UserService;
 import java.util.Arrays;
@@ -23,14 +22,12 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-public class User {
+public class UserTest {
   @Autowired
   private MockMvc mockMvc;
 
   @MockBean
   private UserService userService;
-  @Autowired
-  private ObjectMapper objectMapper;
 
   @Test
   public void FindAllShouldReturnAllUsers() throws Exception {
@@ -51,4 +48,30 @@ public class User {
     result.andExpect(jsonPath("$[*].email").exists());
   }
 
+  @Test
+  public void FindUsersByIdShouldReturnUsersById() throws Exception {
+    long userId1 = 1L;
+    long userId2 = 2L;
+
+    UserDTO user1 = new UserDTO(userId1, "Rafael", "rafael@email.com");
+    UserDTO user2 = new UserDTO(userId2, "Thiago", "thiago@email.com");
+
+    when(userService.findById(userId1)).thenReturn(user1);
+    when(userService.findById(userId2)).thenReturn(user2);
+
+    ResultActions resultUser1 = mockMvc.perform(get("/user/{id}", userId1).contentType(MediaType.APPLICATION_JSON));
+    ResultActions resultUser2 = mockMvc.perform(get("/user/{id}", userId2).contentType(MediaType.APPLICATION_JSON));
+
+    resultUser1
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(userId1))
+        .andExpect(jsonPath("$.name").value(user1.getName()))
+        .andExpect(jsonPath("$.email").value(user1.getEmail()));
+
+    resultUser2
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(userId2))
+        .andExpect(jsonPath("$.name").value(user2.getName()))
+        .andExpect(jsonPath("$.email").value(user2.getEmail()));
+  }
 }
